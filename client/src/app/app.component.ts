0c4dc925ad74a70de51bd64cb7020e8ea4c6bf3f
Webcloudxx <a.beshovishki@gmail.com>
2025-08-19 09:40:27 +0300

    you can view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <nav style="display:flex; gap:12px; padding:10px 0;">
+      <a routerLink="/">Catalog</a>
+      <a routerLink="/search">Search</a>
+      <a routerLink="/login">Login</a>
+      <a routerLink="/register">Register</a>
+      <a routerLink="/me">My Posts</a>
+      <a routerLink="/create">Create</a>
+    </nav>
+    <hr />
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'client';
-}
+export class AppComponent {}
